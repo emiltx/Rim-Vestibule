f011597dda1596d10b778ff2e7d914bf2fcfbe4e
emiltx <crocomile@gmail.Com>
2026-07-23 00:52:27 +0200

    add : vestibule room

---
+++ Source/RoomVestibule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Vestibule
+{
+    public class RoomRoleWorker_Vestibule : RoomRoleWorker
+    {
+        public override float GetScore(Room room)
+        {
+            List<Thing> things = room.ContainedAndAdjacentThings;
+
+            bool hasLocker = things.Any(t => t.TryGetComp<CompEntryWardrobe>() != null);
+            int doorCount = things.OfType<Building_Door>().Distinct().Count();
+
+            if (hasLocker && doorCount >= 2)
+            {
+                return 100000f;
+            }
+            return 0f;
+        }
+    }
+}
